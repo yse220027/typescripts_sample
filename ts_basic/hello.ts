c099c28ed2c95cf9433c45474693ae5a6aeea9ae
yse220027 <220027gs@yse-c.net>
2023-11-29 11:36:12 +0900

    update

--- ts_basic/hello.ts
+++ ts_basic/hello.ts
@@ -1,6 +1,33 @@
-var message:string = "Hello!";
+var message:string = "Hello!!!!";
 console.log(message);
 
-//方推論(type Inference)
+// var id;
+// var email;
+// var isActive;
+// var none;
+
+// 型推論(Type Inference)
 var id = 1;
-var email = "test@test.com"
+var email = "test@test.com";
+var isActive = true;
+var none;
+
+// id = "YSE";
+// email = 1000;
+// isActive = "OK";
+// none = 1000;
+// none = "YSE";
+
+// 型定義(Type definition)
+// var id:number = 1;
+// var email:string = "test@test.com";
+// var isActive:boolean = true;
+// var none:any;
+
+none = "YSE";
+none = 1;
+
+console.log(id, typeof id)
+console.log(email, typeof email)
+console.log(isActive, typeof isActive)
+console.log(none, typeof none)
